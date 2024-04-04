2acb07b5f0ef15c1c5e0b840b27aed2676f78555
Omar_Ameer <omar.ameer244@gmail.com>
2024-04-05 01:04:47 +0200

    Home Repo Done.

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -1,7 +1,9 @@
 // Here, this is abstract as in thic class I will determine what will happen, but without determining how that will happen.
+import 'package:bookly_app/Core/errors/failure.dart';
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeauredBooks();
-  Future<List<BookEntity>> fetchNewewstBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeauredBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewewstBooks();
 }
