7dc91f708144766e299dd57cc8a32b185c01bccf
Omar_Ameer <omar.ameer244@gmail.com>
2024-04-06 20:08:27 +0200

    Home remote data source done.

--- lib/Features/home/data/data_sources/home_remote_data_sourse.dart
+++ lib/Features/home/data/data_sources/home_remote_data_sourse.dart
@@ -2,6 +2,8 @@
 but without determining how that will happen.
 Also, I will not use the either type here; as I don't need to handle the exceptions here.
 */
+import 'package:bookly_app/Core/utils/api_service.dart';
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
 
 abstract class HomeRemoteDataSource {
@@ -10,15 +12,32 @@ abstract class HomeRemoteDataSource {
 }
 
 class HomeRemoteDataSourceImpl implements HomeRemoteDataSource {
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImpl(this.apiService);
   @override
-  Future<List<BookEntity>> fetchFeauredBooks() {
-    // TODO: implement fetchFeauredBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchFeauredBooks() async {
+    var data = await apiService.get(
+      endPoint: 'volumes?Filtering=free-ebooks&q=programming',
+    );
+    List<BookEntity> books = getBooksList(data);
+    return books;
   }
-  
+
   @override
-  Future<List<BookEntity>> fetchNewewstBooks() {
-    // TODO: implement fetchNewewstBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchNewewstBooks() async {
+    var data = await apiService.get(
+      endPoint: 'volumes?Filtering=free-ebooks&Sorting=newewst&q=programming',
+    );
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    for (var bookMap in data['items']) {
+      books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
   }
 }
