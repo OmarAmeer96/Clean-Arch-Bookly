4ab91dee8c9433f558d1e683c1e3e92c8bb76bfc
Omar_Ameer <omar.ameer244@gmail.com>
2024-04-09 01:16:43 +0200

    Hive [3] - Cache featured books data.

--- lib/Features/home/data/data_sources/home_remote_data_sourse.dart
+++ lib/Features/home/data/data_sources/home_remote_data_sourse.dart
@@ -3,8 +3,10 @@ but without determining how that will happen.
 Also, I will not use the either type here; as I don't need to handle the exceptions here.
 */
 import 'package:bookly_app/Core/utils/api_service.dart';
+import 'package:bookly_app/Core/utils/functions/cache_books_data_after_fetching_it_from_api.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/constants.dart';
 
 abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> fetchFeauredBooks();
@@ -21,6 +23,7 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
       endPoint: 'volumes?Filtering=free-ebooks&q=programming',
     );
     List<BookEntity> books = getBooksList(data);
+    cacheBooksDataAfterFetchingItFromApi(books, kFeaturedBox);
     return books;
   }
 
