38e0ad896b70b5e611ed01ef589b221da31b49d8
Omar_Ameer <omar.ameer244@gmail.com>
2024-04-09 01:21:33 +0200

    Hive [4] - Initilize hive.

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   runApp(const BooklyApp());
+  await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
   await Hive.openBox(kFeaturedBox);
 }
