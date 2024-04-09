7482e8cee587ecdaa177c2edcb222f272993ec62
Omar_Ameer <omar.ameer244@gmail.com>
2024-04-09 18:21:13 +0200

    Update dependencies in pubspec.yaml

--- lib/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,7 +1,7 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'featured_books_state.dart';
 
