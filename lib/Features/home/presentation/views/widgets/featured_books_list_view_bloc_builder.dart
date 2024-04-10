6b18549ef95ec76529a062d6198c92267625aaf4
Omar_Ameer <omar.ameer244@gmail.com>
2024-04-10 17:53:36 +0200

    [Pagination 4] - Add pagination support to FeaturedBooksCubit and FeaturedBooksListViewBlocBuilder

--- lib/Features/home/presentation/views/widgets/featured_books_list_view_bloc_builder.dart
+++ lib/Features/home/presentation/views/widgets/featured_books_list_view_bloc_builder.dart
@@ -1,24 +1,36 @@
+import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class FeaturedBooksListViewBlocBuilder extends StatelessWidget {
+class FeaturedBooksListViewBlocBuilder extends StatefulWidget {
   const FeaturedBooksListViewBlocBuilder({
     super.key,
   });
 
   @override
+  State<FeaturedBooksListViewBlocBuilder> createState() =>
+      _FeaturedBooksListViewBlocBuilderState();
+}
+
+class _FeaturedBooksListViewBlocBuilderState
+    extends State<FeaturedBooksListViewBlocBuilder> {
+  List<BookEntity> allBooks = [];
+
+  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+    return BlocConsumer<FeaturedBooksCubit, FeaturedBooksState>(
+      listener: (BuildContext context, FeaturedBooksState state) {
+        if (state is FeaturedBooksSuccess) {
+          allBooks.addAll(state.books);
+        }
+      },
       builder: (context, state) {
-        if (state is FeaturedBooksLoading) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (state is FeaturedBooksSuccess) {
+        if (state is FeaturedBooksSuccess ||
+            state is FeaturedBooksPaginationLoading) {
           return FeaturedBooksListView(
-            books: state.books,
+            books: allBooks,
           );
         } else if (state is FeaturedBooksFailure) {
           return Center(
